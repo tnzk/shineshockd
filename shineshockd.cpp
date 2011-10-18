36849865c5d5fdc8978d9add49e96f40909434a7
tnzk <tnzk.marge@gmail.com>
2011-10-19 07:29:55 +0900

    initial

---
+++ shineshockd.cpp
@@ -0,0 +1,55 @@
+#include<opencv/cv.h>
+#include<opencv/highgui.h>
+#include<iostream>
+#include<stdio.h>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, char** argv)
+{
+  IplImage *src = 0, *tmpl = 0;
+  IplImage *ftmp;
+  IplImage *copy_img;
+  double min_value, max_value;
+  CvPoint min_loc, max_loc;
+  CvFont font;
+  char* text = "hoge";
+  CvSize text_size;
+  
+  src = cvLoadImage("image.bmp", CV_LOAD_IMAGE_COLOR);
+  tmpl = cvLoadImage("template.bmp", CV_LOAD_IMAGE_COLOR);
+
+  if(src == 0 || tmpl == 0){
+    return -1;
+  }
+
+  cvInitFont(&font, CV_FONT_HERSHEY_SIMPLEX, 1.0, 1.0, 0, 4, 4);
+  
+  ftmp = cvCreateImage(cvSize(src->width - tmpl->width + 1, src->height - tmpl->height + 1), 32, 1);
+    
+  cvMatchTemplate(src, tmpl, ftmp, CV_TM_CCOEFF);
+  cvMinMaxLoc(ftmp, &min_value, &max_value, &min_loc, &max_loc, NULL);
+  cvRectangle(src, max_loc, cvPoint(max_loc.x + tmpl->width, max_loc.y + tmpl->height), CV_RGB(0, 244, 255), 3);
+
+  //  cvGetTextSize("*", &font, &text_size, 0);
+  //  cvPutText(src, "*", cvPoint (x, y + text_size.height), &font, CV_RGB(255 , 0 , 255));
+  printf("%i\n", ftmp->widthStep);
+  printf("%i, %i\n", max_loc.x, max_loc.y);
+  printf("%f\n", max_value);
+  for( int y = 0; y < ftmp->height; y++) {
+    for( int x = 0; x < ftmp->width; x++) {
+      CvScalar s = cvGet2D(ftmp,y,x);
+      int xx = x + 100 - 1;
+      int yy = y + 100 - 1;
+      cvRectangle(src, cvPoint(xx,yy), cvPoint(xx + 1, yy + 1), CV_RGB(s.val[0],0,0), 1);
+      if(s.val[0] > 1100000000) printf( "%i, %i: %20.2f\n", x, y , s.val[0]);
+    }
+  }
+
+  cvSaveImage("result.jpg", src);
+  cvSaveImage("result_f.jpg", ftmp);
+
+  return 0;
+}
+
